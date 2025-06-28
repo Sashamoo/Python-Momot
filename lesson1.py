6bf138003b538940164f5f9bf4ff0c7a71f08a71
Sashamoo <sashamomot8045@gmail.com>
2025-06-28 11:53:43 +0300

    add

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,78 @@
-start = int(input("first number" ))
-finish = int(input("second namber"))
+#1
+# start = int(input("first number" ))
+# finish = int(input("second namber"))
 
-for number in range(start, finish + 1):
-    print(number)
+# for number in range(start, finish + 1):
+#     print(number)
+
+
+#2
+# start = int(input("first number" ))
+# finish = int(input("second namber"))
+
+# for number in range(start, finish + 1):
+#     if number % 2 != 0:
+#         print(number)
+
+
+#3
+# start = int(input("first number" ))
+# finish = int(input("second namber"))
+
+# for number in range(finish, start - 1, -1):
+#     if number % 2 == 0:
+#         print(number)
+
+
+#4
+# start = int(input("Введіть перше число: "))
+# end = int(input("Введіть друге число: "))
+
+
+# order = int(input("Оберіть порядок:\n1 — у зростаючому порядку\n2 — у спадному порядку\nВаш вибір: "))
+
+# print("Результат:")
+
+# if order == 1:
+#     for i in range(min(start, end), max(start, end) + 1):
+#         print(i)
+# elif order == 2:
+#     for i in range(max(start, end), min(start, end) - 1, -1):
+#         print(i)
+# else:
+#     print("Неправильний вибір порядку.")
+
+
+#5
+# start = int(input("Введіть перше число: "))
+# end = int(input("Введіть друге число: "))
+
+
+# low = min(start, end)
+# high = max(start, end)
+
+
+# for number in range(low, high +1):
+#     if number % 2 != 0:
+#         print(number)
+
+
+#6
+# a = int(input("Введіть перше число: "))
+# b = int(input("Введіть друге число: "))
+
+# start = min(a, b)
+# end = max(a, b)
+
+# print("Парні числа у зростаючому порядку:")
+# for i in range(start, end + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+# print("Непарні числа у спадному порядку:")
+# for i in range(end, start - 1, -1):
+#     if i % 2 != 0:
+#         print(i)
+
+#2.1
+
