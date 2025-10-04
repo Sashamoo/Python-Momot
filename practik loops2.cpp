b8f9bd6fd520073d486b22d2226a066dbe4d40bd
Sashamoo <sashamomot8045@gmail.com>
2025-10-04 11:26:55 +0300

    add

---
+++ practik loops2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Завдання 1 */
+   
+    /*
+    {
+        int width, height;
+        char ch;
+
+        cout << "Завдання 1: Прямокутник заповнений\n";
+        cout << "Введіть ширину: ";
+        cin >> width;
+        cout << "Введіть висоту: ";
+        cin >> height;
+        cout << "Введіть символ: ";
+        cin >> ch;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                cout << ch;
+            }
+            cout << endl;
+        }
+    }
+
+    cout << endl; // розділення між завданнями
+    */
+
+    /* Завдання 2 */
+    
+    /*
+    {
+        int width, height;
+        char ch;
+
+        cout << "Завдання 2: Прямокутник порожній\n";
+        cout << "Введіть ширину: ";
+        cin >> width;
+        cout << "Введіть висоту: ";
+        cin >> height;
+        cout << "Введіть символ: ";
+        cin >> ch;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                    cout << ch;
+                } else {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+    */
+
+    return 0;
+}
+
